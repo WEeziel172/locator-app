425aa30dd00dbbee28b05ec3f1057b4ebb63a7ae
samuelhakala <samuel.hakala1@gmail.com>
2024-02-26 15:40:59 +0200

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'; // https://vitejs.dev/config/
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: './locator-app',
   test: {
     globals: true,
     environment: 'jsdom',
