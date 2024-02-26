8ebe3b1078913db5ab7f36b8e9dc2a19fb228b53
samuelhakala <samuel.hakala1@gmail.com>
2024-02-26 15:45:34 +0200

    fix: base url

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Home } from '@pages/home/home.tsx';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Home />,
+    },
+  ],
   {
-    path: '/',
-    element: <Home />,
+    basename: import.meta.env.PROD ? '/locator-app/' : '',
   },
-]);
+);
