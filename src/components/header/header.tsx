bc1ad507457414a00f4256d13833759b8ef2e193
samuelhakala <samuel.hakala1@gmail.com>
2024-02-26 03:24:23 +0200

    refactor and fixes

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,12 @@
 import RebelIcon from '@assets/icons/starwars-rebel.svg';
+import { useTranslation } from 'react-i18next';
 
 export function Header() {
+  const { t } = useTranslation();
   return (
     <header className={'h-24 flex items-center justify-start glass'}>
       <img src={RebelIcon} className={'h-28 w-28'} />
-      <h1 className={'text-amber-50 text-2xl'}>Rebel Locator App</h1>
+      <h1 className={'text-amber-50 text-2xl'}>{t('header')}</h1>
     </header>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { router } from './router.tsx';
 import { RouterProvider } from 'react-router-dom';
+import './i18n/config.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
