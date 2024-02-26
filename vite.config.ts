258c2afd9ec2db7ad667a676a54ee112eca37a95
samuelhakala <samuel.hakala1@gmail.com>
2024-02-26 18:34:19 +0200

    fix: base url prod check

--- vite.config.ts
+++ vite.config.ts
@@ -4,52 +4,54 @@ import { resolve } from 'path';
 import react from '@vitejs/plugin-react'; // https://vitejs.dev/config/
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'prod' ? '/locator-app/' : '/',
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts',
-  },
-  resolve: {
-    alias: [
-      {
-        find: '@pages',
-        replacement: resolve(__dirname, './src/pages'),
-      },
-      {
-        find: '@hooks',
-        replacement: resolve(__dirname, './src/hooks'),
-      },
-      {
-        find: '@services',
-        replacement: resolve(__dirname, './src/services'),
-      },
-      {
-        find: '@customTypes',
-        replacement: resolve(__dirname, './src/types'),
-      },
-      {
-        find: '@components',
-        replacement: resolve(__dirname, './src/components'),
-      },
-      {
-        find: '@constants',
-        replacement: resolve(__dirname, './src/constants.ts'),
-      },
-      {
-        find: '@config',
-        replacement: resolve(__dirname, './src/config.ts'),
-      },
-      {
-        find: '@stores',
-        replacement: resolve(__dirname, './src/stores'),
-      },
-      {
-        find: '@assets',
-        replacement: resolve(__dirname, './src/assets'),
-      },
-    ],
-  },
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/locator-app/' : '/',
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/tests/setup.ts',
+    },
+    resolve: {
+      alias: [
+        {
+          find: '@pages',
+          replacement: resolve(__dirname, './src/pages'),
+        },
+        {
+          find: '@hooks',
+          replacement: resolve(__dirname, './src/hooks'),
+        },
+        {
+          find: '@services',
+          replacement: resolve(__dirname, './src/services'),
+        },
+        {
+          find: '@customTypes',
+          replacement: resolve(__dirname, './src/types'),
+        },
+        {
+          find: '@components',
+          replacement: resolve(__dirname, './src/components'),
+        },
+        {
+          find: '@constants',
+          replacement: resolve(__dirname, './src/constants.ts'),
+        },
+        {
+          find: '@config',
+          replacement: resolve(__dirname, './src/config.ts'),
+        },
+        {
+          find: '@stores',
+          replacement: resolve(__dirname, './src/stores'),
+        },
+        {
+          find: '@assets',
+          replacement: resolve(__dirname, './src/assets'),
+        },
+      ],
+    },
+  };
 });
